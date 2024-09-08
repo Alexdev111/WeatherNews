ea48181c68556902192566f936ee25a762af0ff9
Alexandru Gurgurov <gurgurov.alexandru001@gmail.com>
2024-09-08 18:42:14 +0300

    Actualizare sfârșit de linie pentru index.js

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-import React from "react"
+import React from "react";
 import * as ReactDOMClient from 'react-dom/client';
-import App from "./App"
-import './style.css'
+import App from "./App";
+import './style.css';
 
-const app = ReactDOMClient.createRoot(document.getElementById("app"))
+const app = ReactDOMClient.createRoot(document.getElementById("app"));
 
-=======
-import React from "react"
-import * as ReactDOMClient from 'react-dom/client';
-import App from "./App"
-import './style.css'
-
-const app = ReactDOMClient.createRoot(document.getElementById("app"))
-
->>>>>>> f996f703f13b8070038c91f759ddc9391b5b9116
-app.render(<App />)
+app.render(<App />);
